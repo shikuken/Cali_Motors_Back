0d03cc61d862d0cabfbb1ed3b2694e210a4eafa8
Tab000 <thanosntm2007@gmail.com>
2026-04-16 19:13:16 -0500

    Swagger y boton de eliminar

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
@@ -12,6 +14,9 @@ app.use(cors({
 
 app.use(express.json({ limit: '4mb' }))
 app.use(express.urlencoded({ extended: true, limit: '4mb' }))
+
+// Ruta para la documentación de Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
